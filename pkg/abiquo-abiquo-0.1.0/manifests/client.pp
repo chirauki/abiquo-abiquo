9d790cc6c74ea3c56362a83c7e45b1455fbd35e8
Marc Cirauqui <mcirauqui@gmail.com>
2014-02-14 19:14:22 +0100

    New pkg build

--- pkg/abiquo-abiquo-0.1.0/manifests/client.pp
+++ pkg/abiquo-abiquo-0.1.0/manifests/client.pp
@@ -15,8 +15,54 @@ class abiquo::client inherits abiquo {
     notify  => Service['abiquo-tomcat']
   }
 
-  if $uipkg == "abiquo-ui" {
-    include abiquo::apache
+  if $uipkg == "abiquo-ui" {    
+    class { 'apache':
+      default_mods        => false,
+      default_vhost       => false,
+      default_confd_files => false,
+    }
+
+    class { 'apache::mod::dir': }
+    class { 'apache::mod::rewrite': }
+    class { 'apache::mod::proxy': }
+    class { 'apache::mod::proxy_ajp': }
+
+    $proxy_pass = [
+      { 'path' => '/api', 'url' => 'ajp://localhost:8010/api' },
+      { 'path' => '/legal', 'url' => 'ajp://localhost:8010/legal' },
+    ]
+
+    if $secure == true {
+      apache::vhost { 'abiquo-ssl':
+        port            => '443',
+        docroot         => '/var/www/html',
+        ssl             => true,
+        proxy_pass      => $proxy_pass,
+        directories     => [ { path => '/var/www/html/ui', 'options' => 'MultiViews', 'allowoverride' => 'None', 'order' => 'allow,deny', 'allow' => 'from all', 'directoryindex' => 'index.html' } ],
+        rewrites        => [ { rewrite_rule => ['^/$ /ui/ [R]'] } ],
+        require         => Package['abiquo-ui']
+      }
+
+      apache::vhost { 'abiquo-redir':
+        port            => '80',
+        docroot         => '/var/www/html',
+        ssl             => false,
+        rewrites        => [ { rewrite_rule => ['.* https://%{SERVER_NAME}%{REQUEST_URI} [L,R=301]'] } ],
+        require         => Package['abiquo-ui']
+      }
+    }
+    else {
+      apache::vhost { 'abiquo':
+        port            => '80',
+        docroot         => '/var/www/html',
+        ssl             => false,
+        proxy_pass      => $proxy_pass,
+        directories     => [ { path => '/var/www/html/ui', 'options' => 'MultiViews', 'allowoverride' => 'None', 'order' => 'allow,deny', 'allow' => 'from all', 'directoryindex' => 'index.html' } ],
+        rewrites        => [ { rewrite_rule => ['^/$ /ui/ [R]'] } ],
+        require         => Package['abiquo-ui']
+      }
+    }
+
     
     exec { 'Remove old client-premium':
       path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
@@ -28,6 +74,19 @@ class abiquo::client inherits abiquo {
       path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
       command => "sed -i 's/localhost/${::ipaddress}/g' /var/www/html/ui/config/client-config.json",
       onlyif  => "grep localhost /var/www/html/ui/config/client-config.json",
+      require => Package['abiquo-ui']
+    }
+
+    exec { 'Set the right protocol in UI API location':
+      path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
+      command => $secure ? {
+        true  => "sed -i 's/http/https/g' /var/www/html/ui/config/client-config.json",
+        false => "sed -i 's/https/http/g' /var/www/html/ui/config/client-config.json",
+      },
+      onlyif  => $secure ? { 
+        true  => "grep http: /var/www/html/ui/config/client-config.json",
+        false => "grep https: /var/www/html/ui/config/client-config.json",
+      },
       require => Package['abiquo-ui']
     }
   }
--- pkg/abiquo-abiquo-0.1.0/manifests/params.pp
+++ pkg/abiquo-abiquo-0.1.0/manifests/params.pp
@@ -1,8 +1,10 @@
 class abiquo::params {
   $abiquo_version = "2.9"
-  $rollingrepourl = "http://10.60.20.42/anycloud/rpm/"
-  $baserepourl = "http://mirror.abiquo.com/abiquo/2.6/os/x86_64/"
-  $apilocation = "http://${::ipaddress}/api"
+  $rollingrepourl = "http://mirror.abiquo.com/abiquo/2.9/os/x86_64/"
+  #$baserepourl = "http://mirror.abiquo.com/abiquo/2.6/os/x86_64/"
+  $baserepourl = $rollingrepourl
+  $secure = false
+  # For RS
   $apiaddress = "192.168.1.132"
   $rstype = "publiccloud"
 }
