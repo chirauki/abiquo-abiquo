b4b5c28636015c659e084924209a6b08dad03747
Marc Cirauqui <marc.cirauqui@abiquo.com>
2014-11-13 19:58:42 +0100

    Fix legal proxy

--- manifests/client.pp
+++ manifests/client.pp
@@ -43,7 +43,7 @@ class abiquo::client (
 
     $proxy_pass = [
       { 'path' => '/api', 'url' => "ajp://${f_api_address}:8010/api" },
-      { 'path' => '/legal', 'url' => "ajp://${f_api_address}:8010/api" },
+      { 'path' => '/legal', 'url' => "ajp://${f_api_address}:8010/legal" },
     ]
 
     if $secure == true {
